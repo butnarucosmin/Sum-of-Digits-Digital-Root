45619f198b6f47afb38dd2cf305153e11d075e01
Cosmin Butnaru <cosminbutnaru18@gmail.com>
2019-10-11 12:18:14 +0300

    Added Instructions

--- UnitTest.Sum of Digits.Digital Root/NumberTest.cs
+++ UnitTest.Sum of Digits.Digital Root/NumberTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Sum_of_Digits.Digital_Root;
 
